3d78b5eda013434f548b40ceb7af8e6ba4f5e883
LorenaEP1490 <lorenapenialva@gmail.com>
2025-02-05 16:08:56 +0000

    Corrección de CSRF y ALLOWED_HOSTS

--- gestion_vehiculos/settings.py
+++ gestion_vehiculos/settings.py
@@ -108,5 +108,8 @@ LOGIN_REDIRECT_URL = "/"  # Redirigir al inicio después de iniciar sesión
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+    "https://127.0.0.1:8000",
+    "https://localhost:8000",
 ]
+
 
