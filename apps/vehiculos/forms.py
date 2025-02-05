645a1b23524f1362d1dde05b80e4aea928e1e86b
LorenaEP1490 <lorenapenialva@gmail.com>
2025-02-05 16:33:04 +0000

    Creado forms.py en Vehiculos

---
+++ apps/vehiculos/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Vehiculo
+
+class VehiculoForm(forms.ModelForm):
+    class Meta:
+        model = Vehiculo
+        fields = [
+            "dominio",
+            "marca",
+            "modelo",
+            "año",
+            "version",
+            "color_exterior",
+            "kilometros",
+            "tipo_combustible",
+            "transmision",
+            "observaciones",
+        ]
+        widgets = {
+            "dominio": forms.TextInput(attrs={"class": "form-control"}),
+            "marca": forms.TextInput(attrs={"class": "form-control"}),
+            "modelo": forms.TextInput(attrs={"class": "form-control"}),
+            "año": forms.NumberInput(attrs={"class": "form-control"}),
+            "version": forms.TextInput(attrs={"class": "form-control"}),
+            "color_exterior": forms.TextInput(attrs={"class": "form-control"}),
+            "kilometros": forms.NumberInput(attrs={"class": "form-control"}),
+            "tipo_combustible": forms.Select(attrs={"class": "form-control"}),
+            "transmision": forms.Select(attrs={"class": "form-control"}),
+            "observaciones": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
+        }
