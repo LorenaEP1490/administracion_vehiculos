60daa56b3c494248d28835fade02c0b36dfbaac6
LorenaEP1490 <lorenapenialva@gmail.com>
2025-02-05 15:28:00 +0000

    Agregada la página de inicio

--- gestion_vehiculos/urls.py
+++ gestion_vehiculos/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include  # <-- Asegúrate de que 'include' está importado
+from .views import inicio  # Importar la vista de inicio
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     path("gastos/", include("apps.gastos.urls")),  # Gastos
     path("publicaciones/", include("apps.publicaciones.urls")),  # Publicaciones
     path("transferencias/", include("apps.transferencias.urls")),  # Transferencias
+    path("", inicio, name="inicio"),  # Página de inicio
 ]
